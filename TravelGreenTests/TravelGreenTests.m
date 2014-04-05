c3c788269c819fa9e1a4c6a98f841cb213ece8ea
Bess Chan <jesus_loves_you_37@hotmail.com>
2014-04-05 10:11:58 -0400

    Initial Commit

---
+++ TravelGreenTests/TravelGreenTests.m
@@ -0,0 +1,34 @@
+//
+//  TravelGreenTests.m
+//  TravelGreenTests
+//
+//  Created by Bess Chan on 4/5/14.
+//  Copyright (c) 2014 Hackathon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TravelGreenTests : XCTestCase
+
+@end
+
+@implementation TravelGreenTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
